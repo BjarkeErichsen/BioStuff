82c9b71f25a4749e72f70b5f39878c7b2655c7e4
BjarkeErichsen <s204104@dtu.dk>
2022-07-02 03:49:52 +0200

    commit 2 day 1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import torch
 import numpy as np
 print("hello")
 print("hello again")
+
