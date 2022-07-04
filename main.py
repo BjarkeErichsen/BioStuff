d93b85eac2ae70a965a92588920b3a7d7172c819
BjarkeErichsen <s204104@dtu.dk>
2022-07-05 01:00:23 +0200

    commit 3 day 2

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 
 import sys
+#sys.path.append(r"C:\Users\EXG\OneDrive - Danmarks Tekniske Universitet\Skrivebord\BioProgrammering\Data\training_set\training_set")
 import torch
 import extraModule
 import numpy as np
-
+from PIL import Image
+import torchvision.transforms as transforms
+import os
+"""
 for path in sys.path:
     print(path)
-print(torch.cuda.is_available())
+#print(torch.cuda.is_available())
+#extraModule.printer()
 
-extraModule.printer()
+print(Data)
+"""
+
+
+
+
+image = Image.open('iceland.jpg')
+
+
+transform = transforms.Compose([
+    transforms.PILToTensor()])
+transform = transforms.PILToTensor()
+img_tensor = transform(image)
