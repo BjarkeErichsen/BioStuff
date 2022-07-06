88734e335515cf285dcfcfc911eeff92d5b12c4d
BjarkeErichsen <s204104@dtu.dk>
2022-07-06 04:37:13 +0200

    commit 1 day 3

--- extraModule.py
+++ extraModule.py
@@ -2,3 +2,13 @@
 
 def printer():
     print("printing")
+
+import torch
+
+c = torch.tensor([4, 5, 6])
+
+a = [c,c,c]
+
+ab = torch.stack(a, 0)
+
+size = 128, 128
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 
 import sys
-#sys.path.append(r"C:\Users\EXG\OneDrive - Danmarks Tekniske Universitet\Skrivebord\BioProgrammering\Data\training_set\training_set")
 import torch
 import extraModule
 import numpy as np
@@ -16,13 +15,59 @@ for path in sys.path:
 print(Data)
 """
 
+directoryCatsTrain = r"C:\Users\EXG\OneDrive - Danmarks Tekniske Universitet\Skrivebord\BioProgrammering\Data\training_set\training_set\cats"
+directoryDogsTrain = r"C:\Users\EXG\OneDrive - Danmarks Tekniske Universitet\Skrivebord\BioProgrammering\Data\training_set\training_set\dogs"
+directoryCatsTest = r"C:\Users\EXG\OneDrive - Danmarks Tekniske Universitet\Skrivebord\BioProgrammering\Data\test_set\test_set\cats"
+directoryDogsTest = r"C:\Users\EXG\OneDrive - Danmarks Tekniske Universitet\Skrivebord\BioProgrammering\Data\test_set\test_set\dogs"
+
+trainDogsList, trainCatsList, testDogsList, testCatsList = [], [], [], []
+size = 128, 128
+
+for Directory in [directoryCatsTrain, directoryDogsTrain, directoryCatsTest, directoryDogsTest]:
+    for filename in os.listdir(Directory):
+        f = os.path.join(Directory, filename)
+        # checking if it is a file
+        if os.path.isfile(f) and f[-3:] == 'jpg':
+
+            if "training_set\cats" in Directory:
+                image = Image.open(f)  #if image.size != (512, 512):
+                image = image.resize(size)
+
+                transform = transforms.Compose([transforms.PILToTensor()])
+                transform = transforms.PILToTensor()
+                img_tensor = transform(image)
+                trainCatsList.append(img_tensor)
+
+            elif "training_set\dogs" in Directory:
+                image = Image.open(f) #f = location of image
+                image = image.resize(size) #size = tuple of desired size the image sh
+                transform = transforms.Compose([transforms.PILToTensor()])
+                transform = transforms.PILToTensor()
+                img_tensor = transform(image)  #gets tensor
+                trainDogsList.append(img_tensor)
+
+
+            elif "test_set\cats" in Directory:
+                image = Image.open(f)
+                image = image.resize(size)
+                transform = transforms.Compose([transforms.PILToTensor()])
+                transform = transforms.PILToTensor()
+                img_tensor = transform(image)
+                testDogsList.append(img_tensor)
+
+            elif "test_set\dogs" in Directory:
+                image = Image.open(f)
+                image = image.resize(size)
+                transform = transforms.Compose([transforms.PILToTensor()])
+                transform = transforms.PILToTensor()
+                img_tensor = transform(image)
+                testCatsList.append(img_tensor)
+
+trainCats = torch.stack(trainCatsList)    #(image, channels, height, width)
+trainDogs = torch.stack(trainDogsList)
+testDogs = torch.stack(testDogsList)
+testCats = torch.stack(testCatsList)
 
 
 
-image = Image.open('iceland.jpg')
-
-
-transform = transforms.Compose([
-    transforms.PILToTensor()])
-transform = transforms.PILToTensor()
-img_tensor = transform(image)
+trainDogs.shape()
